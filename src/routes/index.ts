4e57874eb24a60449da5878844e96ed6a15c250f
raihan <md.abu.raihan7793@gmail.com>
2025-12-20 21:55:39 +0600

    remove file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router";
 import App from "../App";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-import SignIn from "../pages/SignIn";
 import ForgotPassword from "../pages/ForgotPassword";
 
 const router = createBrowserRouter([
@@ -17,10 +16,6 @@ const router = createBrowserRouter([
       {
         path: "/login",
         Component: Login,
-      },
-      {
-        path: "/sign-in",
-        Component: SignIn,
       },
       {
         path: "/forgot-password",
